440af6bbd54c489e801c29e9b1f4ef05ba3aec37
kush-13 <33835351+kush-13@users.noreply.github.com>
2019-01-31 02:13:54 +0530

    Add files via upload

---
+++ face+smile+eye recogition.py
@@ -0,0 +1,37 @@
+# Importing the libraries
+import cv2
+
+# Loading the cascades
+FACECASCADE="path of the face_cascade.xml file here"
+EYECASCADE="path of the eye_cascade.xml file here"
+SMILECASCADE="path of the smile_cascade.xml file here"
+face_cascade = cv2.CascadeClassifier(FACECASCADE)
+eye_cascade = cv2.CascadeClassifier(EYECASCADE)
+smile_cascade = cv2.CascadeClassifier(SMILECASCADE)
+
+# Defining a function that will do the detections
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
+    return frame
+
+# Doing some Face Recognition with the webcam
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+    cv2.imshow('Video', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
